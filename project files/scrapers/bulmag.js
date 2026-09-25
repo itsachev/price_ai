a653ea92249db3a86abc53eed1906421f29bc8d0
itsachev <tsachev.official@gmail.com>
2026-09-25 20:32:07 +0300

    Delete project files directory

--- project files/scrapers/bulmag.js
+++
@@ -1,9 +0,0 @@
-import { scrapeFromKolkostruva } from './kolkostruva.js';
-
-export const competitorKey = 'bulmag';
-
-// BulMag (ТРЪНЧЕВ ООД, mainly NE Bulgaria) submits to the KZP "Колко струва"
-// daily open-data ZIP, keyed by company EIK.
-export function scrape(supabase) {
-  return scrapeFromKolkostruva(supabase, competitorKey, '127585839');
-}
--- project files/scrapers/hitmax.js
+++
@@ -1,9 +0,0 @@
-import { scrapeFromKolkostruva } from './kolkostruva.js';
-
-export const competitorKey = 'hitmax';
-
-// Hit Max (ХИТ ХИПЕРМАРКЕТ ЕООД) submits to the KZP "Колко струва" daily
-// open-data ZIP, keyed by company EIK.
-export function scrape(supabase) {
-  return scrapeFromKolkostruva(supabase, competitorKey, '131016929');
-}
